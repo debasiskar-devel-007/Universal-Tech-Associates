63bc8993684cec9a4b7de24021dc446b23b7b409
influxiq1 <himadribar.influxiq@gmail.com>
2020-04-29 11:17:38 +0530

    first commit

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('pece-doctor-signup app is running!');
+    expect(page.getTitleText()).toEqual('Universal-Tech-Associates app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,11 +13,10 @@ declare var $: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // template: '',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  /*title = 'nexgetest';*/
+  /*title = 'Universal Tech Associates';*/
 
   public url;
   public loading: boolean = false;
